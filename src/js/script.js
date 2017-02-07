cbaf3ea09e8844fc88db0cffd6d3912251047df0
Julia Ugarkina <ugarkina@rambler.ru>
2017-02-07 20:00:59 +0300

    applied owl caroussel in promo and tried making tabs with Jquery

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
-  // весь ваш код
+  $("#promo-slider-thumbs").owlCarousel({
+    items:1,
+    // center:true,
+    loop:true,
+    // responsive: {
+    //     0: {
+    //         items:3
+    //     },
+    //     768:{
+    //         items: 4
+    //     },
+    //     1200: {
+    //         items:6
+    //     },
+    // }
+  });
+
+  $("#tabs-family").on('click', function(e){
+    e.preventDefault();
+    $ (".tabs__item").toggleClass('tabs__item--active');
+    var changeImgUrl = $(this).attr('href');
+    // $('#gallery-main-img').attr('src', changeImgUrl);
+    console.log(changeImgUrl)
+  });
 });
